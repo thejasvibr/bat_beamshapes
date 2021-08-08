2bcdc702990e6cba932b7bec8f4d72c5f269ed16
thejasvi <thejasvib@gmai.com>
2021-08-08 07:31:48 +0200

    include the script used to make the directivity figures for the preprint into the examples [travis skip]

--- examples/0_preprint_figures.py
+++ examples/0_preprint_figures.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Makes a comparison of  piston in an infinite baffle with piston in a sphere and 
-oscillating cap of a sphere.
+4 models at a time
+==================
+This examples shows how to generate directivity patterns for all 4 currently
+implemented source models. 
 
-The plot is meant to go in the preprint associated with the paper
+We'll run the 4 models for the same `ka` values :math:`ka=1,5,10`  and compare the patterns. 
+
+Important to note
+~~~~~~~~~~~~~~~~~
+As you may notice, some of the models require numerical inputs of a particular format (mpmath float for instance). 
+This is important to pay attention to, and may otherwise lead to avoidable errors. 
+
 """
 import mpmath
 import numpy as np 
@@ -33,10 +41,6 @@ for j,each_ka in enumerate(ka_values):
     
 #%% 
 # Now calculate the piston in sphere 
-# Remember this particular example uses the python-FLINT implementation to speed
-# things up. Check the other examples to see how to generate the piston in a sphere
-# using the mpmath implementation.
-
 pistonsphere_directivity = piston_sphere.piston_in_sphere_directivity
 angles_as_acb = [acb(each) for each in angles]
 
@@ -77,11 +81,6 @@ for j, each_ka in enumerate(ka_values):
     k_value = each_ka/R
     parameters = {'k':k_value, 'R':R}
     _, point_on_sphere[:,j] = point_source_on_a_sphere_directivity(angles, parameters)
-#%%
-plt.figure()
-a0 = plt.subplot(111, projection='polar')
-for i in range(3):
-    plt.plot(angles, point_on_sphere[:,i])
 
 #%%
 
@@ -108,6 +107,10 @@ plt.yticks([-12,-24,-36,-48],['',-24,'', -48], fontsize=6)
 
 plt.xticks(np.radians(tick_angles) ,tick_labels , fontsize=6)
 a0.xaxis.set_tick_params(pad=-3.5)
+#plt.legend(loc=(0.95,-0.07), frameon=False)
+#plt.text(0.05, 1.02, 'piston in a sphere', transform=a0.transAxes, fontsize=8)
+# plt.text(0.55, 1.02, 'piston in an infinite baffle', transform=a0.transAxes,
+#                          multialignment='right', fontsize=8)
 set_ylim()
 plt.text(subplot_labelx, subplot_labely, 'A', transform=a0.transAxes,
                          fontsize=8)
@@ -131,6 +134,10 @@ plt.xticks(np.radians(tick_angles) ,tick_labels , fontsize=6)
 #a1.tick_params(axis='both', pad=0.5)
 a1.xaxis.set_tick_params(pad=-3.5)
 plt.legend(loc=(-0.27,-0.23), frameon=False, fontsize=7)
+#plt.text(0.1, 1.02, 'cap of sphere', transform=a1.transAxes, fontsize=8)
+#plt.text(0.55, 1.02, 'piston in an infinite baffle', transform=a1.transAxes,
+#                         multialignment='right', fontsize=8)
+#plt.tight_layout()
 
 plt.text(subplot_labelx, subplot_labely, 'B', transform=a1.transAxes,
                          fontsize=8)
@@ -150,6 +157,10 @@ for each in tick_angles:
 
 plt.xticks(np.radians(tick_angles) ,tick_labels , fontsize=6)
 a2.xaxis.set_tick_params(pad=-3.5)
+#plt.legend(loc=(-0.175,-0.07), frameon=False)
+#plt.text(0.1, 1.02, 'cap of sphere', transform=a1.transAxes, fontsize=8)
+#plt.text(0.55, 1.02, 'piston in an infinite baffle', transform=a1.transAxes,                         multialignment='right', fontsize=8)
+#plt.tight_layout()
 
 plt.text(subplot_labelx, subplot_labely, 'C', transform=a2.transAxes,
                          fontsize=8)
@@ -168,10 +179,29 @@ for each in tick_angles:
     tick_labels.append(f'{each}'+'$^{\circ}$')
 plt.xticks(np.radians(tick_angles) ,tick_labels , fontsize=6)
 a3.xaxis.set_tick_params(pad=-3.5)
+#plt.legend(loc=(-0.175,-0.07), frameon=False)
+#plt.text(0.1, 1.02, 'cap of sphere', transform=a1.transAxes, fontsize=8)
+#plt.text(0.55, 1.02, 'piston in an infinite baffle', transform=a1.transAxes,                         multialignment='right', fontsize=8)
+#plt.tight_layout()
 set_ylim()
 plt.text(subplot_labelx, subplot_labely, 'D', transform=a3.transAxes,
                          fontsize=8)
-plt.savefig('piston_sphere_baffle.png', bbox_inches='tight', dpi=600)
+
+plt.savefig('../docs/source/_static/piston_sphere_baffle.png', bbox_inches='tight', dpi=600)
+
+
+
+#%%
+#.. image:: ../_static//piston_sphere_baffle.png
+#  :width: 400
+
+#%%
+# Directivity patterns :math:`(D_{\theta}/D_{0})` in dB of the currently implemented sound sources for a common set of ka
+# values for comparison, where k is the wavenumber (:math:`\frac{2 \pi\times f}{c}`) and `a` is the piston radius - or its equivalent. The
+# directivity pattern shows the ratio between the off-axis sound level at (θ◦) to the on-axis level at (0◦) in
+# decibels. A) piston in an infinite baffle B) piston in a sphere with the half-angle aperture α = 30 ◦ , and where
+# the piston radius `a = R sin α` C) oscillating cap of a sphere with α = 30 ◦ , and the equivalent piston radius
+# is `a = R sin α` D) vibrating point on a sphere, here the `kR = ka` for comparison with the other models.
 
 
 
